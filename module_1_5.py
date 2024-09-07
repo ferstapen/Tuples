d3dd59fab41be12469cded0648e08c7a8fa0fcda
ferstapen <164868955+ferstapen@users.noreply.github.com>
2024-09-07 19:29:39 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,13 @@
+# task 2
+immutable_var = 56, 77, 'urban', ['123', 0]
+print(immutable_var)
+
+# task 3
+# immutable_var[3] = True
+# Кортеж является неизменяемым типом данных
+# поэтому нельзя добавлять, изменять или удалять элементы кортежа
+
+# task 4
+mutable_list = [3, 5, False, 'python']
+mutable_list[2] = True
+print(mutable_list)
